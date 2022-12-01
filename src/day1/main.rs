44aa4b0b727e83041edbc5f970ea45ec363e04f8
Adrien Thébault <adrien.thebault-prestataire@laposte.fr>
2022-12-01 17:40:51 +0100

    reorg

---
+++ src/day1/main.rs
@@ -0,0 +1,24 @@
+use std::{cmp::Reverse, str::FromStr};
+
+fn main() {
+    let input = String::from_utf8_lossy(include_bytes!("input.txt"));
+
+    let groups = input.split("\n\n");
+    let mut groups_sum = groups
+        .into_iter()
+        .map(|g| {
+            g.split('\n')
+                .into_iter()
+                .map(usize::from_str)
+                .map(|r| r.expect("invalid usize"))
+                .sum()
+        })
+        .collect::<Vec<usize>>();
+    groups_sum.sort_by_key(|x| Reverse(*x));
+
+    let exo1 = groups_sum.iter().max().expect("no max found");
+    let exo2 = groups_sum.iter().take(3).sum::<usize>();
+
+    println!("{}", &exo1);
+    println!("{}", &exo2);
+}
